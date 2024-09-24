6577bfd49165fe1f36beab4a34321ff59dfc4bf6
hemilrajpura-simform <hemil.r@simformsolutions.com>
2024-09-24 18:49:21 +0530

    feat: cache the static files

--- public/sw.js
+++ public/sw.js
@@ -11,3 +11,41 @@ self.addEventListener("push", function (event) {
 
   event.waitUntil(self.registration.showNotification("Simon Says", options));
 });
+
+const CACHE_NAME = "cache-files";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/static/css/main.css",
+  "/sounds/winSound.wav",
+  "/sounds/clickSound.wav",
+  "/sounds/wrongSound.wav",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching audio file");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        console.log("Serving audio from cache:", event.request.url);
+        return response;
+      }
+
+      return fetch(event.request).catch(() => {
+        console.log(
+          "Network unavailable, and no cache for:",
+          event.request.url
+        );
+      });
+    })
+  );
+});
